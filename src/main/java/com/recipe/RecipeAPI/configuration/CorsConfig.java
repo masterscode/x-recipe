b7c190b8f791c3dcf9111644f866f466532ef9b7
emmanuel ogbinaka <ogbinakaemmanuel@gmail.com>
2022-08-16 12:36:58 +0100

    add cors config

---
+++ src/main/java/com/recipe/RecipeAPI/configuration/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.recipe.RecipeAPI.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfiguration(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
